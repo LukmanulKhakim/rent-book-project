1d31f14283b8a4ca8b92dd4bad91cbda049b5a4d
lukmanul <klukmanul33@gmail.com>
2022-09-26 15:21:30 +0700

    preparation

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func connectGorm() (*gorm.DB, error) {
+	dsn := "root:@tcp(127.0.0.1:3306)/latihan_db?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+
+}
